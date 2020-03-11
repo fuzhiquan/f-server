e7dce36e1c04af7590cf08f1001ce480bc827327
fuzhiquan <fuzhiquan@beisen.com>
2020-03-11 17:50:11 +0800

    refactor: 添加注释

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class Server {
     }
     async handlerRequest(req, res) {
         let { pathname } = url.parse(req.url)
-        pathname = decodeURIComponent(pathname) // 防止中文
+        pathname = decodeURIComponent(pathname) // 防止url中有中文
 
         const absPath = path.join(__dirname, pathname)
         try{
@@ -31,10 +31,10 @@ class Server {
         }catch(e) {
             if(req.headers.origin) {
                 res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-                res.setHeader('Access-Control-Allow-Credentials', 'true')
+                res.setHeader('Access-Control-Allow-Credentials', 'true') //允许请求携带cookie
                 res.setHeader('Access-Control-Allow-Headers', '*')
                 res.setHeader('Access-Control-Max-Age', '1800') // 设置OPTIONS预检最大存活时间，单位s,一般三分钟
-                res.setHeader('Access-Control-Allow-Methods', 'PUT,DELETE,OPTIONS')
+                res.setHeader('Access-Control-Allow-Methods', 'PUT,DELETE,OPTIONS') // 默认支持get，post
                 res.setHeader('Set-Cookie', 'name=zhi;SameSite=None;Secure')
                 if(req.method === 'OPTIONS') {
                     return res.end()
@@ -71,7 +71,7 @@ class Server {
     hasCache(statObj, absPath, req, res) {
         // 获取文件最后一次修改时间
         const ctime = statObj.ctime.toUTCString()
-        // 强制缓存
+        // 强制缓存，Expires 设置绝对时间，Cache-Control 设置相对时间
         res.setHeader('Expires', new Date(Date.now() + 3600 * 24 * 1000).toUTCString())
         res.setHeader('Cache-Control', 'max-age=3600 * 24')
         // 对比缓存
@@ -80,10 +80,11 @@ class Server {
         const content = readFileSync(absPath, 'utf8')
         const hash = crypto.createHash('md5').update(content).digest('base64')
         res.setHeader('Etag', hash)
-
+        // 如果第一次访问时服务端返回了Last-Modified头，那么再次请求时，请求头会带着is-modified-since，我们可以对比时间是否改变
         if(req.headers['is-modified-since'] !== ctime) {
             return false
         }
+        // 如果第一次访问时服务端返回了Etag, 再次请求，请求头会带if-none-match
         if(req.headers['if-none-match'] !== hash) {
             return false
         }
@@ -94,6 +95,16 @@ class Server {
         if(this.hasCache(statObj, absPath, req, res)) {
             res.statusCode = 304
             res.end()
+        }
+        // 处理206
+        const range = req.headers['range']
+        if(range) {
+            let [, start, end] = range.match(/\b(\d+)-(\d+)\b/)
+            start = Number(start)
+            end = Number(end)
+            res.statusCode = 206
+            res.setHeader('Content-Range', `bytes ${start}-${end}/${statObj.size}`)
+            return createReadStream(absPath, {start, end}).pipe(res)
         }
         // 处理压缩
         const encoding = req.headers['accept-encoding']
